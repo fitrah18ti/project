a65d8f767cd60720094fcf7d066acf8a97b0995a
aness17 <52444655+aness17@users.noreply.github.com>
2019-07-07 22:21:31 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,29 @@
 <?php
 $day  = date("l");
 $next = date('l' , strtotime('now + 1days'));
+
+switch($day){
+ case 1:
+  $hari : "Monday";
+  break;
+ case 2:
+  $hari : "Tuesday";
+  break;
+  case 3:
+  $hari : "Wednesday";
+  break;
+ case 4:
+  $hari : "Thursday";
+  break;
+ case 5:
+  $hari : "Friday";
+  break;
+ case 6:
+  $hari : "Saturday";
+  break;
+ case 7:
+  $hari : "Sunday";
+  break;
+ default :
+  print("End of the world");  
  ?>
